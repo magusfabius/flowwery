a627724bae10cec6677d9fdcf8757a8c43aad8a9
Fabio <fabio.karlino@gmail.com>
2023-03-01 15:03:43 +0100

    vercel tailwind debugging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,css,ts,jsx,tsx}"
+    "./src/**/*.{html,js,css,ts,jsx,tsx}",
+    "./src/pages/components/**/*.{html,js,css,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
