120a1f67acfe737e6ae5c8d85d07570b10b0ff5d
Robb <ruberto.riccardo@gmail.com>
2023-03-02 17:53:14 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -223,7 +223,7 @@ export default function Home() {
       </Head>
 
       <main>
-      <nav id="header" className="fixed w-full z-10 top-0">
+      <nav id="header" className="fixed w-full z-10 top-0 bg-gradient-to-b from-white to-transparent">
 
         <div className="w-full md:max-w-4xl mx-auto flex flex-wrap items-center justify-between mt-0 py-3">
 
